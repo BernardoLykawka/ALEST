f14de7d032c67e1e2494412d72911117c629829f
Bernardo Lykawka <bernardolmsilvas@gmail.com>
2024-08-20 12:51:02 -0300

    Update App.java

--- App.java
+++ App.java
@@ -4,8 +4,11 @@ public class App {
         
         int[] a = { 1, 3, 10, 15, 5 };
         int index=a.length-1;
+        System.out.println("Soma de elementos do array: ");
         System.out.println("somar com for: "+r.somarFor(a));
         System.out.println("somar com recursao:"+r.somarRec(a,index));
+        System.out.println("---------------------------");
+
     }
 
 
